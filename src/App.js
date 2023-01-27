6f7259a7a57bf0fdb7f8695a014ad94ef6de36ed
Ivan Garcia <hectorivang2000@gmail.com>
2023-01-27 11:31:12 -0500

    pruebas entrevista

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios"
+
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [datos,setDatos]= React.useState([]);
+
+  React.useEffect(  () => {
+    axios.get('https://csoct9fwak.execute-api.us-east-1.amazonaws.com/Stage/api/v1/other_charges').then((response) => {
+      setDatos(response.data);
+    });
+    }, [])
+
+
+    console.log(datos);
+    return (
+      <div>
+        <div>
+        <h1>Lista de personas</h1>
+        </div>
+        <div>
+        <ul  >      
+                  {datos.map((persona)=>{
+                  return(<li key={persona.ID} >{persona.name}</li>
+                  )
+          }) 
+              }</ul>
+
+        </div>
+             
+    
+      </div>
+    );
+}
 export default App;
